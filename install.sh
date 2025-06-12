743fcef9236673f994433a079f26191b9423612f
TJ Taylor <dugancathal@gmail.com>
2025-06-12 16:17:46 -0600

    Remove jrnl install

--- install.sh
+++ install.sh
@@ -16,5 +16,4 @@ git clone https://github.com/dugancathal/dotfiles "${DOTFILES_DIR}"
 
 cd $DOTFILES_DIR
 bundle check || bundle
-bundle exec rake install:merge_install install:asdf install:tmuxifier install:jrnl
-
+bundle exec rake install:merge_install install:asdf install:tmuxifier
